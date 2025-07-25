7aed2a6ae8841e8900d1206876220a19e039a239
suraj211223 <sd862768@gmail.com>
2025-07-25 22:37:01 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ContactForm from '../components/ContactForm';
+import ContactForm from 'src/components/ContactForm';
 import BackgroundShapes from '../components/shapes';
 import SuccessPopup from '../components/popup';
 import { ArrowLeft } from 'lucide-react';
